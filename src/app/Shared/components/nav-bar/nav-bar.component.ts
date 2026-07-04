0f2887ba1553ccbe126a25ee6040abed94a3e109
Snehal Patil. <patilsnehal1469@gmail.com>
2026-07-04 17:58:22 +0530

    ok

--- src/app/Shared/components/nav-bar/nav-bar.component.ts
+++ src/app/Shared/components/nav-bar/nav-bar.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../../services/user.service';
 import { ProductService } from '../../services/product.service';
 import { Router } from '@angular/router';
 import { FairsService } from '../../services/fairs.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,36 +12,43 @@ import { FairsService } from '../../services/fairs.service';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor(private userservice : UserService,
-              private productservice : ProductService,
-              private fairservice : FairsService,
-              private router : Router
+  constructor(private userservice: UserService,
+    private productservice: ProductService,
+    private fairservice: FairsService,
+    private router: Router,
+    private authservice: AuthService
   ) { }
 
   ngOnInit(): void {
   }
 
-  ongoproducts(){
+  ongoproducts() {
     this.productservice.fetchproducts().subscribe(res => {
-      this.router.navigate(['/products',res[0].pid],{
-        queryParams : {cr : res[0].canReturn}
+      this.router.navigate(['/products', res[0].pid], {
+        queryParams: { cr: res[0].canReturn }
       })
     })
 
   }
-  ongousers(){
+  ongousers() {
     this.userservice.fetchuser().subscribe(res => {
-      this.router.navigate(['/users', res[0].userId],{
-        queryParams : {cr : res[0].userRole }
+      this.router.navigate(['/users', res[0].userId], {
+        queryParams: { cr: res[0].userRole }
       })
 
     })
 
   }
 
-  gettofairs(){
+  gettofairs() {
     this.fairservice.fetchfairs().subscribe(res => {
       this.router.navigate(['/fairs', res[0].fairId])
     })
+  }
+
+  onlogout() {
+    this.authservice.LogOut()
+    this.router.navigate([''])
+
   }
 }
